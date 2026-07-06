27a3ae0f8b330d2435169286dd3006d287354ea8
Ty Torres <tiedye3.141@gmail.com>
2026-07-05 21:37:14 -0700

    fixed the window working again but not the shader

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,14 @@
 #include "renderloop/rendercontext.hpp"
 #include <string>
 #include "geometry/object.hpp"
+#include "error.hpp"
 
 #define SHADER_PATH "C:/Users/Ty/Downloads/GitHub/OpenGL-First-Project/src/shaders/"
 
 int main()
 {
+    Window w{WIDTH, HEIGHT, MAJOR, MINOR};
+
     std::string Path{SHADER_PATH};
     //this will eventually be the mesh class and an object class 
     float vertices[] = {
@@ -23,27 +26,27 @@ int main()
     };  
     
     unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO); 
-
+    glGenVertexArrays(1, &VAO);
+    
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glBindVertexArray(0); //unbind
-
+    
+    
     std::string vert{Path + "main.vert"};
     std::string frag{Path + "main.frag"};
     Shader s(vert.c_str(), frag.c_str());
-    Window w{WIDTH, HEIGHT, MAJOR, MINOR};
+    
     RenderContext RomanCancel{w};
 
-    Object o;
-    o.shape->VAO = VAO;
-    o.shape->VBO = VBO;
-    o.shape->EBO = 0;
-    o.look->s = &s;
-    RomanCancel.addObject(o);
+    // Object o;
+    // o.shape->VAO = VAO;
+    // o.shape->VBO = VBO;
+    // o.shape->EBO = 0;
+    // o.look->s = &s;
+    // RomanCancel.addObject(o);
     RomanCancel.renderLoop();
 }
